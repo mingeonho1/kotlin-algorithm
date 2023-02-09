0e8b375b8079f85e75ddbea6bbc0e8607a44d4f1
GEON_HO <102270909+mingeonho1@users.noreply.github.com>
2023-02-09 15:57:26 +0900

    2023-02-09

---
+++ src/main/kotlin/level0/문자열안에 문자열.kt
@@ -0,0 +1,20 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-02-09
+ * **/
+
+/** 문제 설명
+문자열 str1, str2가 매개변수로 주어집니다.
+str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요. **/
+
+private fun solution(str1: String, str2: String) = if (str1.contains(str2)) 1 else 2
+
+
+fun main() {
+    val str1 = "ab6CDE443fgh22iJKlmn1o"
+    val str2 = "6CD"
+    val result = 1
+    checkAnswer(solution(str1, str2) == result)
+}
---
+++ src/main/kotlin/level0/자릿수 더하기.kt
@@ -0,0 +1,18 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-02-09
+ * **/
+
+/** 문제 설명
+정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요 **/
+
+private fun solution(n: Int) = n.toString().map { it.digitToInt() }.sum()
+
+
+fun main() {
+    val n = 930211
+    val result = 16
+    checkAnswer(solution(n) == result)
+}
