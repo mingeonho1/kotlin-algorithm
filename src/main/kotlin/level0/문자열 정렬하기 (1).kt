0c2df3e0b5d177bfc694e38a93618e8711045193
GEON_HO <102270909+mingeonho1@users.noreply.github.com>
2023-02-14 19:38:02 +0900

    2023-02-14

---
+++ src/main/kotlin/level0/문자열 정렬하기 (1).kt
@@ -0,0 +1,29 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-02-14
+ * **/
+
+/** 문제 설명
+문자열 myString이 매개변수로 주어질 때,
+myString 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요. **/
+
+fun isNumeric(s: String): Boolean {
+    return try {
+        s.toDouble()
+        true
+    } catch (e: NumberFormatException) {
+        false
+    }
+}
+
+private fun solution(myString: String) =
+    myString.replace("[^0-9]".toRegex(), "").map { it.digitToInt() }.toIntArray().sortedArray()
+
+
+fun main() {
+    val myString = "hi12392"
+    val result = intArrayOf(1, 2, 2, 3, 9)
+    checkAnswer(solution(myString).contentEquals(result))
+}
---
+++ src/main/kotlin/level0/암호 해독.kt
@@ -0,0 +1,29 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-02-14
+ * **/
+
+/** 문제 설명
+군 전략가 머쓱이는 전쟁 중 적군이 다음과 같은 암호 체계를 사용한다는 것을 알아냈습니다.
+- 암호화된 문자열 cipher를 주고받습니다.
+- 그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
+문자열 cipher와 정수 code가 매개변수로 주어질 때 해독된 암호 문자열을 return하도록 solution 함수를 완성해주세요 **/
+
+private fun solution(cipher: String, code: Int): String {
+    var answer = ""
+    for (i in code..cipher.length step (code)) {
+        answer += cipher[i - 1]
+    }
+    return answer
+}
+
+
+fun main() {
+    val cipher = "dfjardstddetckdaccccdegk"
+    val code = 4
+    val result = "attack"
+
+    checkAnswer(solution(cipher, code) == result)
+}
