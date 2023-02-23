355aa45a36edb86fc868ce3ef6dc3d6e36feec2f
HOYA <als77770@naver.com>
2023-02-23 14:32:21 +0900

    2023-02-23

---
+++ src/main/kotlin/level0/외계행성의 나이.kt
@@ -0,0 +1,23 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-02-23
+ * **/
+
+/** 문제 설명
+우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다.
+입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다.
+a는 0, b는 1, c는 2, ..., j는 9입니다.
+예를 들어 23살은 cd, 51살은 fb로 표현합니다.
+나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요. **/
+
+private fun solution(age: Int) = age.toString().map { (it.code + 49).toChar() }.joinToString("")
+
+
+fun main() {
+    val age = 23
+    val result = "cd"
+    
+    checkAnswer(solution(age) == result)
+}
---
+++ src/main/kotlin/level0/피자 나눠 먹기 (2).kt
@@ -0,0 +1,23 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-02-23
+ * **/
+
+/** 문제 설명
+머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다.
+피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때,
+n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요. **/
+
+fun gcd(a: Int, b: Int): Int = if (b != 0) gcd(b, a % b) else a
+
+private fun solution(n: Int) = (n * 6 / gcd(n, 6)) / 6
+
+
+fun main() {
+    val n = 10
+    val result = 5
+
+    checkAnswer(solution(n) == result)
+}
