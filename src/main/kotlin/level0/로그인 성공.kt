8b5662c27d6594b0b08a1c8b9aedf0c9226dc191
HOYA <als77770@naver.com>
2023-03-14 22:52:59 +0900

    2023-03-14
    재귀함수 공부하자 ..

---
+++ src/main/kotlin/level0/로그인 성공.kt
@@ -0,0 +1,40 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-03-14
+ * **/
+
+/** 문제 설명
+머쓱이는 프로그래머스에 로그인하려고 합니다.
+머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때,
+다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+
+아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를,
+아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다. **/
+
+private fun solution(idPw: Array<String>, db: Array<Array<String>>): String {
+    var answer = ""
+    for (item in db) {
+        if (idPw[0] == item[0] && idPw[1] == item[1]) {
+            return "login"
+        }
+        if (idPw[0] == item[0]) {
+            answer = "wrong pw"
+        }
+    }
+    if (answer == "") {
+        answer = "fail"
+    }
+    return answer
+}
+
+fun main() {
+    val idPw = arrayOf("programmer01", "15789")
+    val db =
+        arrayOf(arrayOf("programmer02", "111111"), arrayOf("programmer00", "134"), arrayOf("programmer01", "1145"))
+    val result = "wrong pw"
+
+    checkAnswer(solution(idPw, db) == result)
+}
---
+++ src/main/kotlin/level0/치킨 쿠폰.kt
@@ -0,0 +1,32 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-03-14
+ * **/
+
+/** 문제 설명
+프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다.
+쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다.
+시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요. **/
+
+private fun solution(chicken: Int): Int {
+    var coupon = chicken
+    var eatToChicken = 0
+    while (coupon > 10) {
+        val newChicken = coupon / 10
+        eatToChicken += newChicken
+        coupon = newChicken + coupon % 10
+    }
+    val newChicken = coupon / 10
+    eatToChicken += newChicken
+
+    return eatToChicken
+}
+
+fun main() {
+    val chicken = 1081
+    val result = 120
+
+    checkAnswer(solution(chicken) == result)
+}
