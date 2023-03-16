b535cf01c3402ad34398e86923b8c9d7d85eff58
HOYA <als77770@naver.com>
2023-03-17 00:27:45 +0900

    2023-03-16

---
+++ src/main/kotlin/level0/유한소수 판별하기.kt
@@ -0,0 +1,50 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-03-16
+ * **/
+
+/** 문제 설명
+소수점 아래 숫자가 계속되지 않고 유한개인 소수를 유한소수라고 합니다.
+분수를 소수로 고칠 때 유한소수로 나타낼 수 있는 분수인지 판별하려고 합니다.
+유한소수가 되기 위한 분수의 조건은 다음과 같습니다.
+
+기약분수로 나타내었을 때, 분모의 소인수가 2와 5만 존재해야 합니다.
+두 정수 a와 b가 매개변수로 주어질 때, a/b가 유한소수이면 1을, 무한소수라면 2를 return하도록 solution 함수를 완성해주세요. **/
+
+private fun solution(a: Int, b: Int): Int {
+    val gc = gcd(a, b)
+    val denominator = b / gc
+    val set = factorization(denominator)
+    for (s in set) {
+        when (s) {
+            2, 5 -> println("good")
+            else -> return 2
+        }
+    }
+    return 1
+}
+
+private fun factorization(a: Int): MutableSet<Int> {
+    var num = a
+    val list = mutableSetOf<Int>()
+    var i = 2
+    while (num >= 2) {
+        if (num % i == 0) {
+            list.add(i)
+            num /= i
+        } else {
+            i++
+        }
+    }
+    return list
+}
+
+fun main() {
+    val a = 30
+    val b = 21
+    val result = 2
+
+    checkAnswer(solution(a, b) == result)
+}
---
+++ src/main/kotlin/level0/특이한 정렬.kt
@@ -0,0 +1,27 @@
+package level0
+
+import kotlin.math.abs
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-03-16
+ * **/
+
+/** 문제 설명
+정수 n을 기준으로 n과 가까운 수부터 정렬하려고 합니다.
+이때 n으로부터의 거리가 같다면 더 큰 수를 앞에 오도록 배치합니다.
+정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요. **/
+
+private fun solution(numList: IntArray, n: Int): IntArray {
+    return numList.sortedWith { a, b ->
+        if (abs(a - n) == abs(b - n)) b.compareTo(a) else abs(a - n).compareTo(abs(b - n))
+    }.toIntArray()
+}
+
+fun main() {
+    val numList = intArrayOf(10000, 20, 36, 47, 40, 6, 10, 7000)
+    val n = 30
+    val result = intArrayOf(36, 40, 20, 47, 10, 6, 7000, 10000)
+
+    checkAnswer(solution(numList, n).contentEquals(result))
+}
