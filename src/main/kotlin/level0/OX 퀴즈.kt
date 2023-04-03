343d7ee16b5f384519182e6eab5ef84bb45e84d6
HOYA <als77770@naver.com>
2023-04-03 22:50:44 +0900

    2023-04-03

---
+++ src/main/kotlin/level0/OX 퀴즈.kt
@@ -0,0 +1,41 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-04-03
+ * **/
+
+/** 문제 설명
+덧셈, 뺄셈 수식들이 'X 연산자 Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다.
+수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요. **/
+
+private fun solution(quiz: Array<String>): Array<String> {
+    var answer: Array<String> = arrayOf<String>()
+    for (q in quiz) {
+        answer += if (q.contains("+")) {
+            val n = q.split(" + ", " = ")
+            if (n[0].toInt() + n[1].toInt() == n[2].toInt()) {
+                "O"
+            } else {
+                "X"
+            }
+        } else {
+            val n = q.split(" - ", " = ")
+            if (n[0].toInt() - n[1].toInt() == n[2].toInt()) {
+                "O"
+            } else {
+                "X"
+            }
+        }
+
+    }
+    return answer
+}
+
+
+fun main() {
+    val quiz = arrayOf("3 - 4 = -3", "5 + 6 = 11")
+    val result = arrayOf("X", "O")
+
+    checkAnswer(solution(quiz).contentEquals(result))
+}
---
+++ src/main/kotlin/level0/분수의 덧셈.kt
@@ -0,0 +1,35 @@
+package level0
+
+/**
+ * © DEV MING 💻
+ * Current date: 2023-04-03
+ * **/
+
+/** 문제 설명
+첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요. **/
+
+//fun gcd(a: Int, b: Int): Int = if (b != 0) gcd(b, a % b) else a
+
+private fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
+    val deno = denom1 * denom2 / gcd(denom1, denom2)
+    val foun = (numer1 * deno / denom1) + (numer2 * deno / denom2)
+
+    val gcd = gcd(deno, foun)
+
+    val de = deno / gcd
+    val fo = foun / gcd
+
+    return intArrayOf(fo, de)
+}
+
+
+fun main() {
+    val numer1 = 9
+    val denom1 = 2
+    val numer2 = 1
+    val denom2 = 3
+    val result = intArrayOf(29, 6)
+
+    checkAnswer(solution(numer1, denom1, numer2, denom2).contentEquals(result))
+}
