bc17d3c79ca10fdefb487abd0ff32e372178bab6
HOYA <102270909+mingeonho1@users.noreply.github.com>
2023-07-12 14:28:19 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 16.66 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(today: String, terms: Array<String>, privacies: Array<String>): IntArray {
+        val termsMap = terms.associate {
+            it.split(" ")[0] to it.split(" ")[1].toInt()
+        }
+
+        val toadyTotal = total(today)
+
+        return privacies.mapIndexedNotNull { index, privacy ->
+            val (date, type) = privacy.split(" ")
+            val day = termsMap[type]!! * 28
+
+            if (toadyTotal >= total(date) + day) index + 1 else null
+        }.toIntArray()
+    }
+
+    fun total(date: String): Int {
+        val (yyyy, mm, dd) = date.split(".").map(String::toInt)
+        return yyyy * 12 * 28 + mm * 28 + dd
+    }
+}
