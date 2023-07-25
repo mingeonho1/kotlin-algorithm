e3c1a5d52e987b4338400a2b1559053f229c91bf
HOYA <102270909+mingeonho1@users.noreply.github.com>
2023-07-25 22:33:12 +0900

    [level 2] Title: 피보나치 수, Time: 24.82 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.kt
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.kt
@@ -1,7 +1,11 @@
 class Solution {
     fun solution(n: Int): Int {
-        val fib = IntArray(n + 1) { 1 }
-        (3..n).forEach { i -> fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567 }
-        return fib[n]
+        if (n < 2) return n
+        var pair = Pair(0, 1)
+
+        (2..n).forEach { _ ->
+            pair = Pair(pair.second, (pair.first + pair.second) % 1234567)
+        }
+        return pair.second
     }
 }
