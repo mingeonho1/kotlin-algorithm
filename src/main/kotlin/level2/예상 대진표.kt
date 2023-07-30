eb168015a575ff86cd2d2c07a3c0f34aa6dc2e30
HOYA <als77770@naver.com>
2023-07-30 23:15:42 +0900

    Update 예상 대진표.kt
    너무 어렵게 생각했나 ...
    그래도 맥락은 비슷한 것 같은데

--- src/main/kotlin/level2/예상 대진표.kt
+++ src/main/kotlin/level2/예상 대진표.kt
@@ -10,20 +10,14 @@ import checkAnswer
 /** 이제 백준 허브로 자세한 내용이 커밋되고 여기는 풀이만 작성 **/
 
 private fun solution(n: Int, a: Int, b: Int): Int {
-    var answer = 1
-    var _n = n
+    var answer = 0
     var _a = a
     var _b = b
 
-    while (_n > 2) {
+    while (_a != _b) {
+        _a = (_a + 1) / 2
+        _b = (_b + 1) / 2
         answer++
-
-        _n /= 2
-        _a = _a / 2 + _a % 2
-        _b = _b / 2 + _b % 2
-        if ((_a / 2 == _b / 2 || _a == _b + 1) && _a / 2 != _b / 2) {
-            return answer
-        }
     }
 
     return answer
